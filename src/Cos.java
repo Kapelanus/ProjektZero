8ab98b9b3c66a173a6c52e36fac8c62919d35406
MJ <michaljavorski@gmail.com>
2018-11-06 16:52:43 +0100

    Mój pierwszy commit

---
+++ src/Cos.java
@@ -0,0 +1,15 @@
+public class Cos {
+    private int number;
+
+    public Cos(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
